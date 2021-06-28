892cab0b5581208991efe0081859d59f8cb3e3f1
Zahra Ali <zahrafali95@gmail.com>
2021-06-28 18:48:13 -0400

    port change

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,8 @@ function getBundleAnalyzerPlugin(name) {
 const config = {
     devServer: {
         https: true,
-        host: '127.0.0.1',
+        host: '0.0.0.0',
+        port: 3000,
         inline: true,
         proxy: {
             '/': {
